a2fea02cd43a63b184f3870f744d840c109f6282
1F10210096 <s1F102100962@iniad.org>
2024-01-29 23:38:55 +0900

    テスト

--- client/src/utils/login.ts
+++ client/src/utils/login.ts
@@ -22,7 +22,7 @@ import { signOut } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
+console.log(process.env.REACT_APP_FIREBASE_API_KEY);
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,

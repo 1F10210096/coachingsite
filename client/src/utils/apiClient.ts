2de188862ebef42f1c4a9d69f832b984c269ca14
1F10210096 <s1F102100962@iniad.org>
2024-02-02 03:54:35 +0900

    test

--- client/src/utils/apiClient.ts
+++ client/src/utils/apiClient.ts
@@ -3,7 +3,7 @@ import api from 'api/$api';
 import axios from 'axios';
 
 // 環境変数からAPIのベースURLを取得する（指定がない場合はデフォルトURLを使用）
-const baseURL = (process.env.API_BASE_URL ?? "") || 'http://localhost:31577/api';
+const baseURL = (process.env.API_BASE_URL ?? '') || 'http://localhost:31577/api';
 
 // Axiosインスタンスを作成し、ベースURLとwithCredentials設定を行う
 const axiosInstance = axios.create({ baseURL, withCredentials: true });

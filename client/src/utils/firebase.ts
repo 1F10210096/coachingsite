42e93828484fc789987b3b457eddedce55d0ac8a
1F10210096 <s1F102100962@iniad.org>
2024-02-02 05:32:36 +0900

    wdsadwsa

--- client/src/utils/firebase.ts
+++ client/src/utils/firebase.ts
@@ -3,7 +3,7 @@ import { getApp, getApps, initializeApp } from 'firebase/app';
 import type { Auth, User } from 'firebase/auth';
 import { getAuth, sendEmailVerification, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,

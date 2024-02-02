e8f7597500641d686c435fd295ae7dfa832e732a
1F10210096 <s1F102100962@iniad.org>
2024-02-02 10:38:17 +0900

    Versioning untracked files

--- client/src/utils/apiClient.ts
+++ client/src/utils/apiClient.ts
@@ -2,11 +2,4 @@ import aspida from '@aspida/axios';
 import api from 'api/$api';
 import axios from 'axios';
 
-// 環境変数からAPIのベースURLを取得する（指定がない場合はデフォルトURLを使用）
-const baseURL = (process.env.API_BASE_URL ?? '') || 'http://localhost:31577/api';
-
-// Axiosインスタンスを作成し、ベースURLとwithCredentials設定を行う
-const axiosInstance = axios.create({ baseURL, withCredentials: true });
-
-// Aspidaクライアントを作成
-export const apiClient = api(aspida(axiosInstance));
+export const apiClient = api(aspida(axios.create({ withCredentials: true })));
